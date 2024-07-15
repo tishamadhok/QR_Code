b8abd079cdbe09ded521f5b334b8f61e913bc16a
Tisha Madhok <tishamadhok@Tishas-MacBook-Pro.local>
2024-07-15 13:27:34 -0400

    QR CODE Commit

---
+++ generate_qr.py
@@ -0,0 +1,15 @@
+import qrcode
+
+url = "https://github.com/tishamadhok"
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(url)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("qrcode.png")
+print("QR Code generated and saved as qrcode.png")
